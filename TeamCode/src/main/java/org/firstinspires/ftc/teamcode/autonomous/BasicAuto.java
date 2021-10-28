2e114f435eca227d93cacb5bb50898a2fedc34fa
GameChanger77 <gamer4christ2004@gmail.com>
2021-10-28 09:33:24 -0500

    Changes for basic red autonomous.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BasicAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BasicAuto.java
@@ -1,17 +1,20 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.GlobalTelemetry;
 import org.firstinspires.ftc.teamcode.odometry.MovementManager;
 import org.firstinspires.ftc.teamcode.odometry.OdometryBase;
+import org.firstinspires.ftc.teamcode.odometry.Pose;
 import org.firstinspires.ftc.teamcode.submodules.RobotHardware;
 
+@Autonomous(name="Basic Auto Red", group="main")
 public class BasicAuto extends LinearOpMode {
 
     GlobalTelemetry gt = new GlobalTelemetry(telemetry);
     public RobotHardware robot = new RobotHardware(gt);
-    public OdometryBase gps = new OdometryBase(robot);
+    public OdometryBase gps = new OdometryBase(robot, new Pose(34, 9,0));
     Thread gpsThread = new Thread(gps);
     MovementManager move = new MovementManager(robot, gt, gps);
 
@@ -23,7 +26,8 @@ public class BasicAuto extends LinearOpMode {
         telemetry.addData("/> STATUS:", "INIT COMPLETE");
         waitForStart();
 
-        while (move.goToPoint(-37, 27, 0, .75, 1)){}
+        while (move.goToPose(34 + 27, 27, 45, .75, 1)){}
+        while (move.goToPose(115, 27, 90, .75, 1)){}
 
 
         robot.chassis.stop();
